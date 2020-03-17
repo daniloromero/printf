b75765664da8987702c07f865a4ecb9ec93a1641
daniloromero <1601@holbertonschool.com>
2020-03-17 02:35:14 +0000

    1st version, binary and unsigned int fucntions

---
+++ Adv_func1.c
@@ -0,0 +1,61 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
+/**
+ *print_binary - unsigned int argument is converted to binary
+ *@ list: argument
+ *@buffer: memory storage for output
+ *@size: buffer current position
+ */
+void print_binary(va_list list, char *buffer, unsigned int *size)
+{
+	int m = va_arg(list, unsigned int), i = 0, n , tmp;
+
+	if (m < 0)
+	{
+		m = m * (-1);
+		buffer[*size] = '-';
+		*size += 1;
+	}
+	n = m;
+	while (n > 0)
+	{
+		i++;
+		n = (n / 2);
+	}
+	tmp = i;
+	while  (i > 0)
+	{
+		buffer[*size + i - 1] = (m % 2) + '0';
+		i--;
+		m = (m / 2);
+	}
+	*size += tmp;
+}
+/**
+ *print_unsignedint - print unsigned integers
+ *@list: arguments 
+ *@buffer: memory storage fo output
+ *@size: buffer current position
+ */
+void print_unsignedint(va_list list, char *buffer, unsigned int *size)
+{
+	unsigned int m = va_arg(list, unsigned int), i = 0, n = m, tmp;
+
+	while ((n / 10) > 0)
+	{
+		i++;
+		n = (n / 10);
+	}
+	i++;
+	tmp = i;
+	while (i > 0)
+	{
+		buffer[*size + i - 1] = (m % 10) + '0';
+		i--;
+		m = (m /10);
+	}
+	*size += tmp;
+}
